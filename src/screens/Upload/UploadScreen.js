2db5e89dac72b57166fcdb7d1eae0efbe33294d2
Yash Bhayre <yash.bhayre@tcs.com>
2020-12-16 23:17:48 +0530

    upload screen

--- src/screens/Upload/UploadScreen.js
+++ src/screens/Upload/UploadScreen.js
@@ -1,17 +1,20 @@
 
 import React,{useState} from 'react';
-import {View,StyleSheet,Text,TouchableOpacity,Image,Dimensions} from 'react-native';
+import {View,StyleSheet,Text,TouchableOpacity,Image,Dimensions,ScrollView} from 'react-native';
 import HeaderIcon from '../../HOC/HeaderIcon.js';
 import Video from 'react-native-video';
 import ImagePicker from 'react-native-image-picker';
 import VideoPlayer from 'react-native-video-player';
 import InputField from '../../components/InputField';
-
+import DropDownPicker from 'react-native-dropdown-picker';
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 const UploadScreen = (props) => {
     const [filePath, setFilePath] = useState({});
-    const [desc,setDesc] = useState({})
+    const [talent,setTalent] = useState("Acting");
+    const [socialMedia,setSocialMedia] = useState("")
+    const [follower,setFollower] = useState("")
+    const [desc,setDesc] = useState("")
     const chooseFile = () => {
         let options = {
             title: 'Video Picker', 
@@ -46,16 +49,36 @@ const UploadScreen = (props) => {
           }
         });
       };
-    const handleTextChange =(e) => {
-      setDesc(e)
-      console.warn(e)
+    const handleTextChange =(e,type) => {
+      switch(type) {
+        case 'desc' : {
+          setDesc(e)
+          break;
+        }
+        case 'social' : {
+          setSocialMedia(e)
+          break;
+        }
+        case 'follower' : {
+          setFollower(e)
+          break;
+        }
+        default : {
+
+        }
+      }
     }
      const removeFile = () => {
        setFilePath({})
      }
+     const uploadVideo = () => {
+        if(desc== "" || follower==""||socialMedia=="") {
+         
+        }
+     }
 
     return(
-        <View style={{flex:1}}>
+        <ScrollView style={{flex:1}}>
             
             <View style={styles.uploadView}>
             <Text style={{textAlign:'center',fontSize:24,padding:20}}>Upload A Video</Text>
@@ -70,29 +93,80 @@ const UploadScreen = (props) => {
                 }
             </View>
 
-            <View style={{alignSelf:'center',marginTop:15}}>
-            <Text style={styles.label}>Description</Text>
-            <InputField
-                  placeholderTextColor="#a0aec0"
-                  onChangeText= {e=>{handleTextChange(e)}}                  //onBlur={handleBlur('email')}
-                  // value={desc}
-                 containerStyles={styles.containerStyles}
-                />
-            </View>    
             <View style={{flexDirection:'row',justifyContent:'space-around'}}>
               <TouchableOpacity style={styles.buttons} onPress={chooseFile}>
                 <Text style={styles.btnText}>Upload</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.buttons}>
-                <Text style={styles.btnText}>Submit</Text>
                 </TouchableOpacity>
                 <TouchableOpacity style={styles.buttons}
                   onPress={removeFile}>
                 <Text style={styles.btnText}>Remove</Text>
                 </TouchableOpacity>
             </View>
+
+            <View style={{alignSelf:'center',marginTop:15}}>
+
+             <Text style={styles.label}>Talent</Text>
             
-        </View>
+              <View style={{height:50}}>
+                <DropDownPicker
+                    items={[
+                      {value: 'Acting', label: 'Acting'},
+                      {value: 'Singing', label: 'Singing'},
+                      {value: 'Dancing', label: 'Dancing'},
+                      {value: 'Comedy', label: 'Comedy'},
+                      {value: 'Music', label: 'Music'},
+                      {value: 'Magic', label: 'Magic'},
+                      {value: 'Aerobatics', label: 'Aerobatics'},
+                      {value: 'Others', label: 'Others'},
+                    ]}
+                    defaultValue={talent}
+                    containerStyle={{height:50}}
+                    style={styles.picker}
+                    itemStyle={{
+                      justifyContent: 'flex-start',
+                    }}
+                    dropDownStyle={{backgroundColor: '#fafafa'}}
+                    onChangeItem={(item) => {
+                      setTalent(item.label);
+                    }}
+                  />
+              </View>
+          
+              
+            <Text style={[styles.label,{marginTop:20}]}>Write Something About The Video</Text>
+            <InputField
+                  placeholderTextColor="#a0aec0"
+                  onChangeText= {e=>{handleTextChange(e,"desc")}}                  //onBlur={handleBlur('email')}
+                  value={desc}
+                 containerStyles={styles.containerStyles}
+                />
+
+            <Text style={[styles.label,{marginTop:20}]}>Social Media With Highest Followers</Text>
+             <InputField
+                  placeholderTextColor="#a0aec0"
+                  onChangeText= {e=>{handleTextChange(e,"social")}}                  //onBlur={handleBlur('email')}
+                   value={socialMedia}
+                 containerStyles={styles.containerStyles}
+                />
+             <Text style={[styles.label,{marginTop:20}]}>Follower Count On The platform</Text>
+              <InputField
+                  placeholderTextColor="#a0aec0"
+                  onChangeText= {e=>{handleTextChange(e,"follower")}}                  //onBlur={handleBlur('email')}
+                  value={follower}
+                 containerStyles={styles.containerStyles}
+                />
+            <View style={{flexDirection:'row',justifyContent:'space-around'}}>
+            
+                <TouchableOpacity style={styles.buttons} onPress={()=>uploadVideo()}>
+                <Text style={styles.btnText}>Submit</Text>
+                </TouchableOpacity>
+               
+            </View>
+
+            </View>    
+            
+            
+        </ScrollView>
     )
 }
 
@@ -134,7 +208,12 @@ const styles  = StyleSheet.create ({
        fontSize:18,
        fontWeight:'700',
        marginBottom:10
-     }
+     },
+     picker:{
+      backgroundColor: 'white',
+      borderColor:"#edf2f7",
+      borderRadius:3,
+    }
       
     
 })
